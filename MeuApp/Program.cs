2be59c9c91aee2e71a267ddf16e9d1c88aeca05b
Leandro Lopes <leandro_carlos@uol.com.br>
2022-09-01 17:05:43 -0300

    first commit

---
+++ MeuApp/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// definição do método
+void MeuMetodo()
+{
+    Console.WriteLine("C# é legal");
+}
+
+// Invocação do método
+MeuMetodo();
+
+
+string RetornaNome(string nome, string sobrenome, int idade = 44)
+{
+    return nome + " " + sobrenome + " tem " + idade + " anos ";
+}
+
+var nome = RetornaNome("leandro", "lopes");
+
+Console.WriteLine(nome);
+
+
